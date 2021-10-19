7e9d9cd299bc5f9817276898f1282b6d7a0b8ba4
sasapiaz <piacentile.salvatore@gmail.com>
2021-10-19 20:41:59 +0200

    added insulin to measurement choices

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -12,7 +12,7 @@ class Measurement(models.Model):
     date = models.DateField()
     time = models.TimeField()
     value = models.IntegerField()
-    type = models.CharField(max_length=1, choices=(('G', 'Glucose'), ('H', 'Heartbeat')))
+    type = models.CharField(max_length=1, choices=(('G', 'Glucose'), ('H', 'Heartbeat'), ('I', 'Insulin')))
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Tag(models.Model):
